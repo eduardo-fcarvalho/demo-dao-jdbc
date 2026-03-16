d624d048c2d3079de5561cf93cd83475fb26cd45
eduardo-fcarvalho <fcarvalho.eduardo@gmail.com>
2026-03-16 11:09:00 -0400

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	void main() {
 		Department obj = new Department(1, "Books");
-		IO.println(obj);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", 
+				LocalDate.now(), 3000.0, obj);
+		IO.println(seller);
 	}
 	
 }
