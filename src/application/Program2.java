7074c5e2aed692956e6c332dc318edafcfdb645d
eduardo-fcarvalho <fcarvalho.eduardo@gmail.com>
2026-03-16 13:43:20 -0400

    DepartmentDao delete implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -34,6 +34,12 @@ public class Program2 {
 		departmentDao.update(department);
 		IO.println("Update completed");
 		
+		IO.println("\n=== TEST 5: department delete ===");
+		IO.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		IO.println("Delete completed");
+		
 		sc.close();
 	}
 	
